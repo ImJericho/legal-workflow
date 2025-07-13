5ba46273ed853bac8453e5c1331b9f33ab9a35a1
Vivek Patidar <77988917+ImJericho@users.noreply.github.com>
2025-07-13 14:00:21 +0530

    completing the diffrent n8n workflow for assumption and questions api and updating the prompt here

--- assumption_prompt.py
+++ assumption_prompt.py
@@ -1,19 +1,28 @@
 prompt = f'''
-Your client_info, {{ $json.body.client_info.client_name }} wants to file law suit against {{ $json.body.client_info.respondent_name}}.
+Your client wants to file law suit against {{ $json.body.client_details.respondent_name }}
 Below are the details provided:
 
-- client_info's request: {{ $json.body.client_info.request }}
-- client_info's story: {{ $json.body.client_info.story }}
-- client_info's prayer: {{ $json.body.client_info.prayer }}
+- client's request: {{  $json.body.client_details.client_request }}
+- client's story: {{ $json.body.client_details.client_story }}
+- client's prayer: {{ $json.body.client_details.client_prayer }}
 
 Additional context:
-- Questions: {{ $json.body.aditional_info.questions }}
-- Answers: {{ $json.body.aditional_info.answers }}
+- Questions: {{ $json.body.qna.questions }}
+- Answers: {{ $json.body.qna.answers }}
 
 List any assumptions you are making due to missing or unclear information. 
 Put all the assumptions (e.g. about a date, sum, or fact) in a bullet point format.
+
+
+Output should only consist of assumptions nothing else.
+example
+```
+The respondent is your real brother. 
+Your father died without writing any will.
+You already have given them legal notice.
+```
 '''
 
 system_prompt = '''
-You are an experienced civil litigator preparing to interview a new client_info to clarify the facts and legal issues of their case.
+You are an experienced litigator preparing to interview a new client_info to clarify the facts and legal issues of their case.
 '''
--- questions_prompt.py
+++ questions_prompt.py
@@ -1,12 +1,21 @@
 prompt = f'''
 Your client, has approached me to file a case suit.
 
-Client's request: {{ $json.body.request }}
-Client's prayer: {{ $json.body.prayer }}
+Client's name: {{ $json.body.client_details.client_name }}
+Client's request: {{ $json.body.client_details.client_request }}
+Client's story: {{ $json.body.client_details.client_story }}
+Client's prayer: {{ $json.body.client_details.client_prayer }}
 
-Based on the above information, list specific 2-5 follow-up questions to clarify the case (e.g. dates of each event, supporting documents, witnesses, amounts involved, and exact injuries or losses). Ensure questions cover all details needed to establish the cause of action and jurisdiction
+Based on the above information, list specific 2-4 follow-up questions to clarify the case (e.g. dates of each event, supporting documents, witnesses, amounts involved, and exact injuries or losses). Ensure questions cover all details needed to establish the cause of action and jurisdiction.
 
+Output should only consist of question nothing else.
+example
+```
+What's your relationship with respondent? 
+Where is the disputed land located?
+How much money you are claiming?
+```
 '''
 
 
-system_prompt = '''You are a seasoned civil litigator preparing to interview a new client to clarify their case.'''
+system_prompt = '''You are a seasoned litigator preparing to interview a new client to clarify their case.'''
